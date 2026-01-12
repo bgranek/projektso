d77a7a2cc44c0d91de7e0788637e5ab9007fee97
Bartosz Granek <bartosz.granek@icloud.com>
2026-01-13 00:39:23 +0100

    kierownik v1

--- kierownik.c
+++ kierownik.c
@@ -1,5 +1,53 @@
 #include "common.h"
 
+int shm_id = -1;
+StanHali *stan_hali = NULL;
+
+void obsluga_wyjscia() {
+    if (stan_hali != NULL) {
+        shmdt(stan_hali);
+    }
+}
+
+void obsluga_sigint(int sig) {
+    (void)sig;
+    exit(0);
+}
+
+void inicjalizuj() {
+    key_t klucz_shm = ftok(".", KLUCZ_SHM);
+    SPRAWDZ(klucz_shm);
+
+    shm_id = shmget(klucz_shm, sizeof(StanHali), 0600);
+    SPRAWDZ(shm_id);
+
+    stan_hali = (StanHali*)shmat(shm_id, NULL, 0);
+    if (stan_hali == (void*)-1) {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main() {
+    if (atexit(obsluga_wyjscia) != 0) {
+        perror("atexit");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sigaction sa;
+    sa.sa_handler = obsluga_sigint;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+
+    inicjalizuj();
+
+    printf("Kierownik uruchomiony. PID: %d\n", getpid());
+    stan_hali->pid_kierownika = getpid();
+
+    while (1) {
+        sleep(1);
+    }
+
     return 0;
 }
