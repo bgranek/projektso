b8cd8bf0c7ea17bf44f6ab2b5da09f2dbe10cd40
Bartosz Granek <bartosz.granek@icloud.com>
2026-01-26 02:46:02 +0000

    socket monitor (klient)

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ CC = gcc
 CFLAGS = -Wall -Wextra -D_GNU_SOURCE
 LDFLAGS = -lpthread
 
-TARGETS = main kierownik kasjer pracownik kibic
-OBJS = main.o kierownik.o kasjer.o pracownik.o kibic.o
+TARGETS = main kierownik kasjer pracownik kibic monitor
+OBJS = main.o kierownik.o kasjer.o pracownik.o kibic.o monitor.o
 
 all: $(TARGETS)
 
@@ -21,6 +21,12 @@ pracownik: pracownik.o
 
 kibic: kibic.o
 	$(CC) $(CFLAGS) -o $@ kibic.o $(LDFLAGS)
+
+monitor: monitor.o
+	$(CC) $(CFLAGS) -o $@ monitor.o
+
+monitor.o: monitor.c
+	$(CC) $(CFLAGS) -c $<
 
 %.o: %.c common.h rejestr.h
 	$(CC) $(CFLAGS) -c $<
---
+++ monitor.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SOCKET_MONITOR_PORT 9999
+#define BUFOR_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    int interwal = 2;
+    char *host = "127.0.0.1";
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
+            interwal = atoi(argv[++i]);
+            if (interwal < 1) interwal = 1;
+        } else if (strcmp(argv[i], "-h") == 0 && i + 1 < argc) {
+            host = argv[++i];
+        } else if (strcmp(argv[i], "--help") == 0) {
+            printf("Uzycie: %s [-i interwal] [-h host]\n", argv[0]);
+            printf("  -i interwal  Odswiezanie co N sekund (domyslnie: 2)\n");
+            printf("  -h host      Adres serwera (domyslnie: 127.0.0.1)\n");
+            return 0;
+        }
+    }
+
+    printf("\033[2J");
+    printf("Monitor hali - laczenie z %s:%d (Ctrl+C aby zakonczyc)\n\n", host, SOCKET_MONITOR_PORT);
+
+    while (1) {
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == -1) {
+            perror("socket");
+            sleep(interwal);
+            continue;
+        }
+
+        struct sockaddr_in addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(SOCKET_MONITOR_PORT);
+
+        if (inet_pton(AF_INET, host, &addr.sin_addr) <= 0) {
+            fprintf(stderr, "Nieprawidlowy adres: %s\n", host);
+            close(sock);
+            return 1;
+        }
+
+        if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+            printf("\r\033[K[OFFLINE] Brak polaczenia z hala...");
+            fflush(stdout);
+            close(sock);
+            sleep(interwal);
+            continue;
+        }
+
+        char bufor[BUFOR_SIZE];
+        memset(bufor, 0, sizeof(bufor));
+        int n = recv(sock, bufor, sizeof(bufor) - 1, 0);
+        close(sock);
+
+        if (n <= 0) {
+            sleep(interwal);
+            continue;
+        }
+
+        printf("\033[H\033[2J");
+        printf("+==========================================+\n");
+        printf("|         MONITOR HALI (SOCKET)            |\n");
+        printf("+==========================================+\n\n");
+
+        char *token = strtok(bufor, "|");
+        while (token != NULL) {
+            if (strncmp(token, "HALA", 4) == 0) {
+                printf("Status: ONLINE\n\n");
+            } else if (strncmp(token, "KIBICOW:", 8) == 0) {
+                printf("Kibicow w hali: %s\n", token + 8);
+            } else if (strncmp(token, "VIP:", 4) == 0) {
+                printf("VIP: %s\n", token + 4);
+            } else if (strncmp(token, "POJEMNOSC:", 10) == 0) {
+                printf("Pojemnosc: %s\n", token + 10);
+            } else if (strncmp(token, "BILETY:", 7) == 0) {
+                printf("Bilety: %s\n", token + 7);
+            } else if (strncmp(token, "EWAKUACJA:", 10) == 0) {
+                char *val = token + 10;
+                if (strcmp(val, "TAK") == 0) {
+                    printf("\033[31mEWAKUACJA W TOKU!\033[0m\n");
+                } else {
+                    printf("Ewakuacja: NIE\n");
+                }
+            } else if (strncmp(token, "FAZA:", 5) == 0) {
+                int faza = atoi(token + 5);
+                const char *fazy[] = {"PRZED MECZEM", "MECZ TRWA", "PO MECZU"};
+                printf("Faza: %s\n", fazy[faza]);
+            } else if (token[0] == 'S' && token[1] >= '0' && token[1] <= '7') {
+                if (token[1] == '0') printf("\nSektory:\n");
+                char *dane = strchr(token, ':');
+                if (dane) {
+                    int nr = token[1] - '0';
+                    char *blok = strstr(token, "[B]");
+                    printf("  [%d] %s %s\n", nr, dane + 1, blok ? "\033[33m(ZABLOKOWANY)\033[0m" : "");
+                }
+            }
+            token = strtok(NULL, "|");
+        }
+
+        printf("\n------------------------------------------\n");
+        printf("Odswiezanie co %d sek. | Ctrl+C = wyjscie\n", interwal);
+
+        sleep(interwal);
+    }
+
+    return 0;
+}
